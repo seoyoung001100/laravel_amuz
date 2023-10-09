34968aacf1b1ca761295fee1299b0081f4eb8c24
seoyoung00100 <HIOOWWOOIH@gmail.com>
2023-10-10 08:46:15 +0900

    페이지네이션

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- <link rel="stylesheet" href="css/list.css"> --}}
-    
+    @vite('resources/css/app.css')
     <title>NOTES</title>
     <style>
         /* @import url(resources/css/list.css); */
@@ -150,7 +150,7 @@
                 color: #47372a;
             }
     </style>
-
+</head>
 <body>
     {{-- <h1 class="text-3xl font-bold underline">
         Hello world!
@@ -199,7 +199,56 @@
             
         </div>
     </div> --}}
+
+    <div class="mt-5 my-auto flex justify-around">
+        <nav>
+            <ul class="flex">
+              <li>
+                <a
+                  class="mx-1 flex h-9 w-9 items-center justify-center rounded-full bg-[#9E8A7A] border border-blue-gray-100 bg-transparent p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300"
+                  href="#"
+                  aria-label="Previous"
+                >
+                  <span class="material-icons text-sm"><</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  class="mx-1 flex h-9 w-9 items-center justify-center rounded-full bg-[#9E8A7A] from-pink-600 to-pink-400 p-0 text-sm text-white shadow-md shadow-pink-500/20 transition duration-150 ease-in-out"
+                  href="#"
+                >
+                  1
+                </a>
+              </li>
+              <li>
+                <a
+                  class="mx-1 flex h-9 w-9 items-center justify-center rounded-full bg-[#9E8A7A] border border-blue-gray-100 bg-transparent p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300"
+                  href="#"
+                >
+                  2
+                </a>
+              </li>
+              <li>
+                <a
+                  class="mx-1 flex h-9 w-9 items-center justify-center rounded-full bg-[#9E8A7A] border border-blue-gray-100 bg-transparent p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300"
+                  href="#"
+                >
+                  3
+                </a>
+              </li>
+              <li>
+                <a
+                  class="mx-1 flex h-9 w-9 items-center justify-center rounded-full bg-[#9E8A7A] border border-blue-gray-100 bg-transparent p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300"
+                  href="#"
+                  aria-label="Next"
+                >
+                  <span class="material-icons text-sm">></span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+    </div>
     
-    {{ $contents->links() }}
+    {{-- {{ $contents->links() }} --}}
 </body>
 </html>
